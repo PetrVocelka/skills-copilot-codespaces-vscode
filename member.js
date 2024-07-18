3ae3b4b9d5d8745ccece45066f19605919723c7c
Petr Vocelka <Vocelka.P@gmail.com>
2024-07-18 17:45:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    // member.js
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ['JavaScript', 'React', 'Angular', 'Node', 'Python']
+    }
+    return member;
+}
